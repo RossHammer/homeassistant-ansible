a47c32d345d80697501ec78a2c834dccc651321c
Ross Hammermeister <ross.hammermeister@resolver.com>
2019-05-29 01:01:22 -0600

    Update

--- apply.sh
+++ apply.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-SSH_KEY=~/.ssh/id_rsa
+[ -n "$SSH_KEY" ] || SSH_KEY=~/.ssh/id_rsa
 
 export ANSIBLE_CONFIG=./ansible.cfg
 ansible-playbook -i hosts --vault-password-file=$SSH_KEY main.yml

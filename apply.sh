20c9bd1c46b4c478005c60773b1279f34682f0ba
Ross Hammermeister <ross.hammermeister@resolver.com>
2020-02-17 23:26:21 -0700

    Updating home assistant

--- apply.sh
+++ apply.sh
@@ -3,4 +3,4 @@
 [ -n "$SSH_KEY" ] || SSH_KEY=~/.ssh/id_rsa
 
 export ANSIBLE_CONFIG=./ansible.cfg
-ansible-playbook -i hosts --vault-password-file=$SSH_KEY main.yml
+ansible-playbook -i hosts --vault-password-file=$SSH_KEY main.yml $@
